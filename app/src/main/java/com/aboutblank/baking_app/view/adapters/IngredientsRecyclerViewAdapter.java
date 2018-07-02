decfb0efd5d11a5a34b2c5fedce2909c54386af4
Justin Yang <aboutblank2264@gmail.com>
2018-07-02 20:30:08 +0800

    added icon change for ingredient list

--- app/src/main/java/com/aboutblank/baking_app/view/adapters/IngredientsRecyclerViewAdapter.java
+++ app/src/main/java/com/aboutblank/baking_app/view/adapters/IngredientsRecyclerViewAdapter.java
@@ -1,10 +1,12 @@
 package com.aboutblank.baking_app.view.adapters;
 
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.aboutblank.baking_app.R;
@@ -12,10 +14,14 @@ import com.aboutblank.baking_app.data.model.Ingredient;
 
 import java.util.List;
 
+import butterknife.BindDrawable;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class IngredientsRecyclerViewAdapter extends RecyclerView.Adapter<IngredientsRecyclerViewAdapter.IngredientsViewHolder> {
+    private final static float GREYED = .3f;
+    private final static float NORMAL = 1.0f;
+
     private List<Ingredient> ingredientList;
 
     public IngredientsRecyclerViewAdapter(List<Ingredient> ingredientList) {
@@ -47,28 +53,39 @@ public class IngredientsRecyclerViewAdapter extends RecyclerView.Adapter<Ingredi
     }
 
     public class IngredientsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-//        @BindView(R.id.ingredient_item)
-//        View itemView;
+        @BindView(R.id.ingredient_icon)
+        ImageView iconView;
 
         @BindView(R.id.ingredient_text)
         TextView ingredient;
+
+        @BindDrawable(R.drawable.ic_done_black_24dp)
+        Drawable doneIcon;
+
+        @BindDrawable(R.drawable.ic_add_black_24dp)
+        Drawable addIcon;
 
         IngredientsViewHolder(View view) {
             super(view);
 
             ButterKnife.bind(this, view);
+
+            view.setOnClickListener(this);
         }
 
-        public void setIngredient(Ingredient ingredient) {
+        void setIngredient(Ingredient ingredient) {
             this.ingredient.setText(ingredient.toPrint());
         }
 
+        //TODO store state of clicked ingredients.
         @Override
         public void onClick(View item) {
-            if(item.getAlpha() == 1.0f) {
-                item.setAlpha(.5f);
+            if(item.getAlpha() == NORMAL) {
+                item.setAlpha(GREYED);
+                iconView.setBackground(doneIcon);
             } else {
-                item.setAlpha(1.0f);
+                item.setAlpha(NORMAL);
+                iconView.setBackground(addIcon);
             }
         }
     }
